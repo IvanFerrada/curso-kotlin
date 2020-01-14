b50edaa1d8e8d3b2ea216a178e469c7d2759252a
Ivan Ferrada <ivanmatiasferrada@gmail.com>
2020-01-14 04:43:40 -0300

    hola mundo en kotlin funciones & if y else

---
+++ src/funcion con if & else.kt
@@ -0,0 +1,24 @@
+fun main (args: Array<String>){
+
+    println(mayorEdad(18))
+    println(mayorEdad(10))
+    println(ejemplo(20))
+    println(ejemplo(15))
+}
+
+/* if & else */
+
+
+
+fun mayorEdad (edad: Int) = if (edad >= 18) true else false
+
+/* otro ejemplo*/
+
+fun ejemplo (bloque: Int) = if(bloque >= 18){
+
+    println("hola tenes 18")
+
+}else{
+
+    println("No tenes 18!!")
+}
---
+++ src/funciones- kotlin.kt
@@ -0,0 +1,20 @@
+/* comentario en kotlin */
+
+fun main (args: Array<String>){
+    println("hola mundo")
+
+   println(suma(20,50))
+
+   println(resta(80,40))
+}
+
+fun suma(a : Int, b : Int ): Int{
+    return a + b
+
+}
+
+/* o usar simplificar de esta forma la funcion */
+
+fun resta (e : Int, r : Int) = e -r
+
+
